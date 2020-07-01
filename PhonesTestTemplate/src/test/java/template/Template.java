72e50ca4e873e565f9737cdec1baa9d103ee4593
A476442 <A476442@LAPTOP-I107BKVD>
2020-07-01 17:21:53 +0300

    Add a JUnit Test

---
+++ PhonesTestTemplate/src/test/java/template/Template.java
@@ -0,0 +1,36 @@
+package template;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+	public class Template {
+	  private WebDriver driver;
+	  JavascriptExecutor js;
+		  
+	  @Before
+	  public void setUp() {
+	    driver = new ChromeDriver();
+	  }
+		  
+	  @After
+	  public void tearDown() {
+	    driver.quit();
+	  }
+		  
+	  @Test
+	  public <WebElement> void test() throws InterruptedException {
+	    driver.get("https://www.google.com/");
+	    Thread.sleep(10000);	    
+	    driver.findElement(By.name("q")).click();
+	    driver.findElement(By.name("q")).sendKeys("unify.com");
+	    driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
+	   }
+	  			
+}
