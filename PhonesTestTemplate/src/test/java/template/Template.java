b8409a33794ddbd1cdb29c32677b8db917566643
A476442 <A476442@LAPTOP-I107BKVD>
2020-07-02 15:07:37 +0300

    Working Maven Test start JHPT

--- PhonesTestTemplate/src/test/java/template/Template.java
+++ PhonesTestTemplate/src/test/java/template/Template.java
@@ -3,36 +3,48 @@ package template;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 	public class Template {
-	  private WebDriver driver;
-	  JavascriptExecutor js;
+		
+		private void pause(double seconds) {
+            try {
+                  Thread.sleep((long) (seconds*1000));                 
+            } catch(InterruptedException e) {
+                  Thread.currentThread().interrupt();
+            }
+		}
+		
+		private WebDriver driver;
+			JavascriptExecutor js;
 		  
-	  @Before
-	  public void setUp() {
-	    driver = new ChromeDriver();
-	  }
+			@Before
+				public void setUp() {
+					DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+					ChromeOptions options = new ChromeOptions();
+					options.addArguments("--incognito");
+					options.addArguments("--no-proxy-server");
+					options.addArguments("start-maximized");
+					capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+					driver = new ChromeDriver(capabilities);
+					}
 		  
-	  @After
-	  public void tearDown() {
-	    driver.quit();
-	  }
+
+			@After
+				public void tearDown() {
+					driver.quit();
+					}
 		  
-	  @Test
-	  public <WebElement> void test() throws InterruptedException {
-		Thread.sleep(1000);
-		driver.get("https://www.google.com/");
-		Thread.sleep(1000);
-	    driver.findElement(By.name("q")).click();
-	    driver.findElement(By.name("q")).sendKeys("unify.com");
-	    driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
-	    Thread.sleep(1000);	
-	   }
+			
+			@Test
+				public <WebElement> void test() throws InterruptedException {
+				driver.get("http://10.12.137.252:8090/jhptGui/husimPhoneList.jsf");
+				pause(5);
+				}
+		}
 	  			
-}
+
